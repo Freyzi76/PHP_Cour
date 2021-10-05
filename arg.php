47cfe7e1324ee9f4cfdad281ac7da0e6365f95c5
hugo MARC <hugo.marc76113@gmail.com>
2021-10-05 10:22:39 +0200

    ajout de  arg.php

---
+++ arg.php
@@ -0,0 +1,19 @@
+<?php
+
+Skrom($argv);
+
+
+function Skrom($age) 
+{
+
+    if($age[1] == 1) {
+
+        echo 'Skrom a ' . $age[1] . ' an';
+
+    } else {
+
+        echo 'Skrom a ' . $age[1] . ' ans';
+
+    }
+
+}
